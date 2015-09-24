41f65e87f75b30b0a29f97913f8e403c72c1e9bb
merseyviking <john@geospark.co.uk>
2015-09-24 17:49:20 +0200

    Added missing "blist" package. More PEP8 changes.

--- indigo/cli.py
+++ indigo/cli.py
@@ -7,23 +7,23 @@ from indigo.models import initialise, sync, destroy
 
 from ingest import do_ingest
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Interact with the indigo system')
     parser.add_argument('command', type=str, metavar="N", nargs="+",
-                       help='The command to run')
+                        help='The command to run')
     parser.add_argument('--config', '-c', dest='config', action='store',
-                       help='Specify the location of the configuration file')
+                        help='Specify the location of the configuration file')
     parser.add_argument('--group', '-g', dest='group', action='store',
-                       help='Specify the group name for ingestion of data')
+                        help='Specify the group name for ingestion of data')
     parser.add_argument('--user', '-u', dest='user', action='store',
-                       help='Specify the username for ingestion of data')
+                        help='Specify the username for ingestion of data')
     parser.add_argument('--folder', dest='folder', action='store',
-                       help='Specify the root folder to ingest from on disk')
+                        help='Specify the root folder to ingest from on disk')
     parser.add_argument('--noimport', dest='no_import', action='store_true',
-                       help='Set if we do not want to import the files into Cassandra')
-
+                        help='Set if we do not want to import the files into Cassandra')
     parser.add_argument('--localip', dest='local_ip', action='store',
-                       help='Specify the IP address for this machine (subnets/private etc)')
+                        help='Specify the IP address for this machine (subnets/private etc)')
     return parser.parse_args()
 
 
@@ -31,37 +31,41 @@ def create(cfg):
     initialise(cfg.get("KEYSPACE", "indigo"))
     sync()
 
+
 def zap(cfg):
     keyspace = cfg.get("KEYSPACE", "indigo")
     initialise(keyspace)
     destroy(keyspace)
+
 
 def user_list(cfg):
     from indigo.models import User
     for user in User.objects.all():
         print "Username: {}, ID: {}".format(user.username, user.id)
 
+
 def user_add(cfg, username=None):
     from indigo.models import User
     from getpass import getpass
 
-    if not username:
+    if username is None:
         username = raw_input("Please enter the user's username: ")
     else:
         username = username[0]
-
-    admin = raw_input("Is this an administrator? [y/N] ")
 
     user = User.objects.filter(username=username).first()
     if user:
         print "ERROR: Username {} is already in use".format(username)
         sys.exit(1)
 
+    admin = raw_input("Is this an administrator? [y/N] ")
+
     email = raw_input("Please enter the user's email address: ")
     password = getpass("Please enter the user's password: ")
-    User.create(username=username, password=password, email=email,administrator=(admin.lower()=='y'))
+    User.create(username=username, password=password, email=email, administrator=(admin.lower() == 'y'))
 
     print "Success: User with username {} has been created".format(username)
+
 
 def group_add(cfg, args):
     from indigo.models import Group, User
@@ -79,13 +83,13 @@ def group_add(cfg, args):
 
     print "Created group '{}' with id: {}".format(name, group.id)
 
+
 def group_delete(cfg, args):
-    from indigo.models import Group, User
+    from indigo.models import Group
     if not args or not len(args) == 1:
         print "Error: Group Name is a required parameters"
         sys.exit(0)
 
-    name = args[0]
     group = Group.find(args[0])
     group.delete()
 
@@ -101,11 +105,12 @@ def group_add_user(cfg, args):
     group_name, username = args
     user = User.find(username)
     group = Group.find(group_name)
-    if not group.id in user.groups:
+    if group.id not in user.groups:
         user.groups.append(group.id)
         user.update(groups=user.groups)
 
     print "Added {} to {}".format(user.username, group.name)
+
 
 def group_list(cfg):
     from indigo.models.group import Group
@@ -114,7 +119,8 @@ def group_list(cfg):
         for user in group.get_users():
             print ".ID: {}\tUsername: {}\tAdministrator:{}\tOwner: {}".format(
                 user.id, user.username, ("N", "Y")[user.administrator],
-                ("N", "Y")[user.id==group.owner])
+                ("N", "Y")[user.id == group.owner])
+
 
 def main():
     args = parse_arguments()
