3778d71c549e98ba16916cf75087b4627bffbf57
Gregory Jansen <gregory.jansen@gmail.com>
2018-02-20 16:37:04 -0500

    reliable settings from environment, including comma-separated hostnames or IPs

--- settings.py
+++ settings.py
@@ -6,11 +6,18 @@ def ips(hostnames):
     result = []
     hostnames = hostnames.split(',')
     for v in hostnames:
+        if v == '':
+            continue
         try:
-            ip = socket.gethostbyname(v)
-            result.append(ip)
-        except Exception as e:
-            print(e)
+            socket.inet_aton(v)  # test legal IP
+            result.append(v)
+        except socket.error as e1:  # Not legal IP
+            print(e1)
+            try:
+                ip = socket.gethostbyname(v)  # returns the IP unchanged for an IP
+                result.append(ip)
+            except Exception as e:
+                print(e)
     return result
 
 KEYSPACE = os.getenv('DRASTIC_KEYSPACE', 'drastic')
@@ -18,3 +25,6 @@ LOG_LEVEL = os.getenv('DRASTIC_LOG_LEVEL', 'WARN')
 CASSANDRA_HOSTS = ips(os.getenv('CASSANDRA_HOSTNAMES', 'cassandra-1'))
 REPLICATION_FACTOR = int(os.getenv('CASSANDRA_REPLICATION_FACTOR', '2'))
 CONSISTENCY_LEVEL = int(os.getenv('CASSANDRA_CONSISTENCY_LEVEL', '2'))
+
+if __name__ == '__main__':
+    print('hosts: {0}'.format(str(CASSANDRA_HOSTS)))
